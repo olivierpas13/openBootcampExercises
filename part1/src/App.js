cacaf4e2a796cae8f0c95a6cb872abf6b1fd7b02
Olivier Paspuel <olivier_paspuel@hotmail.com>
2022-07-22 14:22:41 -0500

    Completed exercises 1.3-1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,20 +1,26 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-  const parts = [part1, part2, part3];
-  const numbers = [exercises1, exercises2, exercises3];
-  const total = exercises1 + exercises2 + exercises3;
-
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} numbers={numbers} />
-      <Total numbers={numbers} total={total} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
@@ -23,22 +29,24 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
-const Content = ({ parts, numbers }) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part name={parts[0]} number={numbers[0]} />
-      <Part name={parts[1]} number={numbers[1]} />
-      <Part name={parts[2]} number={numbers[2]} />
+      <Part name={parts[0].name} number={parts[0].exercises} />
+      <Part name={parts[1].name} number={parts[1].exercises} />
+      <Part name={parts[2].name} number={parts[2].exercises} />
     </div>
   );
 };
 
-const Total = ({ numbers, total }) => {
-  console.log(numbers[0]);
-  return <p>Number of exercises {total}</p>;
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  );
 };
-
-export default App;
 
 const Part = ({ name, number }) => {
   return (
@@ -47,3 +55,5 @@ const Part = ({ name, number }) => {
     </p>
   );
 };
+
+export default App;
