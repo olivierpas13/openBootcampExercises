6b108cb6c5a230a0faefc417ae808d3638d281f8
Olivier Paspuel <olivier_paspuel@hotmail.com>
2022-07-22 13:56:48 -0500

    Code refactored into different components

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,22 +6,44 @@ const App = () => {
   const exercises2 = 7;
   const part3 = "State of a component";
   const exercises3 = 14;
+  const parts = [part1, part2, part3];
+  const numbers = [exercises1, exercises2, exercises3];
+  const total = exercises1 + exercises2 + exercises3;
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={parts} numbers={numbers} />
+      <Total numbers={numbers} total={total} />
     </div>
   );
 };
 
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Content = ({ parts, numbers }) => {
+  return (
+    <div>
+      <Part name={parts[0]} number={numbers[0]} />
+      <Part name={parts[1]} number={numbers[1]} />
+      <Part name={parts[2]} number={numbers[2]} />
+    </div>
+  );
+};
+
+const Total = ({ numbers, total }) => {
+  console.log(numbers[0]);
+  return <p>Number of exercises {total}</p>;
+};
+
 export default App;
+
+const Part = ({ name, number }) => {
+  return (
+    <p>
+      {name} {number}
+    </p>
+  );
+};
