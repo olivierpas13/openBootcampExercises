bde36397956936aa8891ed83424b9a8ed3fb10e3
Olivier Paspuel <olivier_paspuel@hotmail.com>
2022-08-06 10:19:34 -0500

    Exercise 4.15 completed, post and get users working with hashed password

--- part4/app.js
+++ part4/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const config = require('./utils/config');
 
 const blogRouter = require('./controllers/blog');
+const userRouter = require('./controllers/user');
 const errorHandler = require('./utils/middleware');
 const logger = require('./utils/logger');
 
@@ -22,6 +23,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
+app.use('/api/users', userRouter);
 app.use('/api/blogs', blogRouter);
 app.use(errorHandler);
 
---
+++ part4/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+});
+
+/*eslint-disable*/
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  },
+});
+/* eslint-enable */
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
