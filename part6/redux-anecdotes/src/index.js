70a6c0c0d01c9eb94b15c7f3392af6ad3d5e443f
Olivier Paspuel <olivier_paspuel@hotmail.com>
2022-08-17 12:05:40 -0500

    6.3 exercise completed

---
+++ part6/redux-anecdotes/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { legacy_createStore as createStore} from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+import reducer from './reducers/anecdoteReducer'
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
