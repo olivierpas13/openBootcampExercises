099cca531ba5ede6cd87b84552649de673dd0636
Olivier Paspuel <olivier_paspuel@hotmail.com>
2022-08-27 18:18:53 -0500

    Exercise 8.16 completed

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,35 +1,14 @@
-const { ApolloServer, gql, UserInputError } = require('apollo-server')
-const {v4: uuid} =  require ('uuid')
-const mongoose = require('mongoose')
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+
+const { ApolloServer, gql, UserInputError, AuthenticationError } = require('apollo-server')
 const Author = require ('./models/author')
 const Book = require ('./models/book')
+const User = require ('./models/user')
+
 const db = require('./db')
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
+const JWT_SECRET = process.env.SECRET
 
 /*
  * Suomi:
@@ -41,59 +20,18 @@ let authors = [
  * However, for simplicity, we will store the author's name in connection with the book
 */
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
 
 const typeDefs = gql`
+
+  type User {
+    username: String!
+    favouriteGenre: String!
+    id: ID!
+  }
+
+  type Token{
+    value: String!
+  }
 
   type Book {
   title: String!
@@ -116,6 +54,7 @@ const typeDefs = gql`
     allBooks(author: String,
     genre: String): [Book]
     allAuthors: [Author]!
+    me: User
   }
 
   type Mutation {
@@ -130,6 +69,16 @@ const typeDefs = gql`
       name: String!
       setBornTo: Int!
     ):Author
+
+    createUser(
+      username: String!
+      favouriteGenre: String!
+    ): User
+
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
 
 `
@@ -137,7 +86,6 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     bookCount: async () =>{
-      // return books.length
       return await Book.collection.countDocuments();
     },
     authorCount: async () => {
@@ -151,10 +99,18 @@ const resolvers = {
     },
     allAuthors: async () =>{
       return Author.find({});
+    },
+    me: async (root, args, context) =>{
+      return context.currentUser
     }
   },
   Mutation:{
-    addBook: async (root, args)=> {
+    addBook: async (root, args, context)=> {
+
+      const {currentUser} = context
+
+      if(!currentUser){throw new AuthenticationError("Not authenticated")}
+
       let author = await Author.findOne({name: args.author})
       
       if(!author){
@@ -163,7 +119,6 @@ const resolvers = {
       author = authorSaved
       }
       
-      console.log(author)
       const book = new Book ({
         ...args,
         author: author._id
@@ -177,7 +132,12 @@ const resolvers = {
         })
       }
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, context) => {
+    
+      const {currentUser} = context
+
+      if(!currentUser){throw new AuthenticationError("Not authenticated")}
+
       const {name, setBornTo} = args
 
       const author = await Author.findOne({name})
@@ -191,7 +151,42 @@ const resolvers = {
         })
       }
 
-       }
+       }, 
+
+    createUser: async(root, args) =>{
+      const user = new User({
+        username: args.username,
+        favouriteGenre: args.favouriteGenre
+      })
+
+
+    
+      try {
+        const saved = await user.save()
+        return saved
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args
+        })
+      }
+    },
+
+    login: async(root, args) => {
+      
+      const user = await User.findOne({ username: args.username })
+      
+      if( !user || args.password !== 'supersecret' ){
+        throw new UserInputError("Wrong credentials")
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+    
+      return {value: jwt.sign(userForToken, JWT_SECRET)}
+
+    }
   },
   Author: {
     bookcount: async (root) => {
@@ -204,6 +199,19 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async({req})=>{
+    const auth = req? req.headers.authorization : null
+
+    if(auth && auth.toLowerCase().startsWith('bearer ')){
+      const {id} = jwt.verify(
+        auth.substring(7), JWT_SECRET
+      )
+
+      const currentUser = await User.findById(id)
+      return {currentUser}
+    }
+
+  }
 })
 
 server.listen().then(({ url }) => {
